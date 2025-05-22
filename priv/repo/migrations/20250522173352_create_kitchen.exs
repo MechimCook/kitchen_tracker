5a8ccd092d0d8deb1e45fa1433d4a03d17b7c907
mechim.cook20@gmail.com <mechim.cook20@gmail.com>
2025-05-22 13:45:29 -0400

    Add migration

---
+++ priv/repo/migrations/20250522173352_create_kitchen.exs
@@ -0,0 +1,15 @@
+defmodule KitchenTracker.Repo.Migrations.CreateKitchen do
+  use Ecto.Migration
+
+  def change do
+    create table(:kitchen) do
+      add :location, :string
+      add :name, :string
+      add :amount, :integer
+      add :unit, :string
+      add :expiration_date, :date
+      add :oldest_entry, :date
+      add :type, :string
+    end
+  end
+end
