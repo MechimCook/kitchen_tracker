e775a6477c06ffaa526e4de86921963472319cab
mechim.cook20@gmail.com <mechim.cook20@gmail.com>
2025-05-23 13:56:36 -0400

    Basic add and check

--- lib/kitchen_tracker.ex
+++ lib/kitchen_tracker.ex
@@ -2,17 +2,35 @@ defmodule KitchenTracker do
   @moduledoc """
   Documentation for `KitchenTracker`.
   """
+   import Ecto.Query, only: [from: 2]
 
-  @doc """
-  Hello world.
+  alias KitchenTracker.Repo
+  alias KitchenTracker.Food
 
-  ## Examples
+  @locations [:freezer, :fridge,  :pantry]
 
-      iex> KitchenTracker.hello()
-      :world
+  def add(location, food_name) do
+    # Check if the location exists
+    if location in @locations do
 
-  """
-  def hello do
-    :world
+      Repo.insert(%Food{location: location, name: food_name})
+
+      # Update the locations
+      :ok
+    else
+      {:error, "Location not found"}
+    end
+  end
+  def check(location) do
+    # Check if the location exists
+    if location in @locations do
+
+      Repo.all(from f in Food, where: f.location == ^location)
+      |> Enum.group_by(& &1.name, & &1.id)
+      |> Enum.map(fn {name, ids} -> {name, length(ids)} end)
+      |> Enum.into(%{})
+    else
+      {:error, "Location not found"}
+    end
   end
 end
--- test/kitchen_tracker_test.exs
+++ test/kitchen_tracker_test.exs
@@ -1,8 +1,16 @@
 defmodule KitchenTrackerTest do
-  use ExUnit.Case
+  use KitchenTracker.RepoCase
   doctest KitchenTracker
 
-  test "greets the world" do
-    assert KitchenTracker.hello() == :world
+  test "Add food stuff" do
+
+    assert KitchenTracker.add(:freezer, "steak") == :ok
+    assert KitchenTracker.check(:freezer) == %{"steak" => 1}
+    assert KitchenTracker.add(:freezer, "steak") == :ok
+    assert KitchenTracker.check(:freezer) == %{"steak" => 2}
+    assert KitchenTracker.add(:freezer, "chicken") == :ok
+    assert KitchenTracker.check(:freezer) == %{"steak" => 2, "chicken" => 1}
+    assert KitchenTracker.add(:freezer, "chicken") == :ok
+    assert KitchenTracker.check(:freezer) == %{"steak" => 2, "chicken" => 2}
   end
 end
